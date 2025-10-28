23cbbd6bdf6a250edcdbcb682b2e49a9f651a55b
unknown <Ali-Hakeem@github.com>
2025-10-28 19:00:22 +0700

    indexjs

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// status LED global
+// layani file di folder public
+app.use(express.static(path.join(__dirname, "public")));
+
 let ledState = false;
 
-// Endpoint untuk melihat status LED
 app.get("/api/status", (req, res) => {
   res.json({ led: ledState });
 });
 
-// Endpoint untuk mengubah status LED (nyala/mati)
 app.post("/api/toggle", (req, res) => {
-  const { state } = req.body; // true / false
+  const { state } = req.body;
   ledState = !!state;
   res.json({ success: true, led: ledState });
 });
 
-// Jalankan server (Vercel otomatis handle port)
-app.listen(3000, () => console.log("Server running on port 3000"));
-
-export default app;
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
