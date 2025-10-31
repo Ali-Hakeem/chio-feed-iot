e7d4e0e20a0bcbab799d47daf268c3a8c4c2c0da
unknown <Ali-Hakeem@github.com>
2025-10-31 19:39:09 +0700

    indexjs

--- index.js
+++ index.js
@@ -13,16 +13,20 @@ app.use(express.json());
 // layani file di folder public
 app.use(express.static(path.join(__dirname, "public")));
 
-let ledState = false;
+// status servo (false = diam, true = berputar)
+let servoAction = false;
 
+// untuk ESP32 mengecek status
 app.get("/api/status", (req, res) => {
-  res.json({ led: ledState });
+  res.json({ rotate: servoAction });
 });
 
-app.post("/api/toggle", (req, res) => {
-  const { state } = req.body;
-  ledState = !!state;
-  res.json({ success: true, led: ledState });
+// tombol di web memicu servo
+app.post("/api/rotate", (req, res) => {
+  servoAction = true;
+  // reset otomatis ke false setelah 3 detik
+  setTimeout(() => (servoAction = false), 3000);
+  res.json({ success: true, rotate: true });
 });
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
